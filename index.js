8bf993667032c1a706072a2c6f06749958ff478e
Molly <norman.j.molly@gmail.com>
2024-12-11 08:33:29 -0600

    Attempt to fix eslint

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 
 // Hardcoded secret key (intentional vulnerability)
 const secretKey = "123456";
+console.log(secretKey);
+
 
 app.get('/', (req, res) => {
     res.send('Welcome to the app!');
